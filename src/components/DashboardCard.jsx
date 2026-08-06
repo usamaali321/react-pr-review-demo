82c1991b0f156e480e3210137a8e49f4b4f0321e
usamaali321 <72518167+usamaali321@users.noreply.github.com>
2026-08-06 17:33:04 +0500

    Update DashboardCard.jsx

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -1,9 +1,17 @@
 export default function DashboardCard({ title, value }) {
   return (
-    <div>
+    <div
+      style={{
+        background: "#ffffff",
+        padding: title === "Users" ? "48px" : "20px",
+        borderRadius: "12px",
+        boxShadow: "0 2px 8px rgba(0,0,0,.1)",
+        margin: "12px"
+      }}
+    >
       <h3>{title}</h3>
-
       <h2>{value}</h2>
+      <button>View Details</button>
     </div>
   );
 }
