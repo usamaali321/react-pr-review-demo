5e620079f2910a1d5679103bba67e722e8011d12
usamaali321 <72518167+usamaali321@users.noreply.github.com>
2026-08-06 16:57:36 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,1 +1,15 @@
+export default function Hero() {
+  return (
+    <section>
+      <h1>Unlimited Movies</h1>
 
+      <p>
+        Watch thousands of movies online.
+      </p>
+
+      <button>
+        Get Started
+      </button>
+    </section>
+  );
+}
