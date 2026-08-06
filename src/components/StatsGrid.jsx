2ace8052bbde0a30ca35949fc5198c165753e915
usamaali321 <72518167+usamaali321@users.noreply.github.com>
2026-08-06 16:58:20 +0500

    Update StatsGrid.jsx

--- src/components/StatsGrid.jsx
+++ src/components/StatsGrid.jsx
@@ -1,1 +1,22 @@
+import DashboardCard from "./DashboardCard";
 
+export default function StatsGrid() {
+  return (
+    <section>
+      <DashboardCard
+        title="Movies"
+        value="2500"
+      />
+
+      <DashboardCard
+        title="Users"
+        value="1.4M"
+      />
+
+      <DashboardCard
+        title="Reviews"
+        value="420K"
+      />
+    </section>
+  );
+}
