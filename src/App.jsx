b48c9349e175b5eda5d4adda3bf887f068e152b6
usamaali321 <72518167+usamaali321@users.noreply.github.com>
2026-08-06 16:56:58 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import StatsGrid from "./components/StatsGrid";
+import Footer from "./components/Footer";
 
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <StatsGrid />
+      <Footer />
+    </>
+  );
+}
